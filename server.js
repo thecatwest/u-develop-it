a65227c4043741ba1959b9f2fa146f099f2e1431
TheCatWest <catwestover@outlook.com>
2021-05-03 15:40:54 -0700

    build database calls

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // import mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // import express
-const express = require('express');
+const express = require("express");
 
 // add PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -14,17 +14,16 @@ app.use(express.json());
 
 // connect application to MySQL database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'poop',
-      database: 'election'
-    },
-    console.log('Connected to the election database.')
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "root",
+    // Your MySQL password
+    password: "poop",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
 );
-
 
 // test the connection with GET test route
 // app.get('/', (req, res) => {
@@ -34,18 +33,46 @@ const db = mysql.createConnection(
 // });
 
 // query the db to test the connection
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(err);
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(err);
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 // add route to handle user requests that aren't supported by app
 // this will override GET routes - make sure it's last of app.uses
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
 // add function that will start express.js server on port 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}!`);
+  console.log(`Server running on port ${PORT}!`);
 });
